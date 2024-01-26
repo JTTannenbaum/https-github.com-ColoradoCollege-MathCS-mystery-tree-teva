942b3d2b5bd355eb0649b1c3f7b23bd1cca1abe4
teva <josephtannebaum@gmail.com>
2024-01-25 21:31:05 -0700

    Initial Commit

---
+++ Swipe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swipe
+//
+//  Created by JTT on 1/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swipe/SwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeApp.swift
+//  Swipe
+//
+//  Created by JTT on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct SwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
